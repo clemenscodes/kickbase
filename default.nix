a9f54e2e7aae419054e50398e668b2aa5eefe824
Clemens Horn <clemens.horn@mni.thm.de>
2024-08-28 20:23:01 +0200

    chore(nix): update cargo hash

--- default.nix
+++ default.nix
@@ -4,6 +4,6 @@ with pkgs;
     pname = "kickbase";
     version = "0.1.1";
     src = ./.;
-    cargoHash = "sha256-TUI3rdNuErqgruLMvCsT2sltPqtaUbicfe9PkGG6fD4=";
+    cargoHash = "sha256-0Erpt5rqYTZqBsM3xhCsliN6h73W5BoTRubFthGEuz8=";
     # cargoHash = "sha256-AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA=";
   }
