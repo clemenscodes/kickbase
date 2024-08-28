4e4e699cfecab6d1bfcc4cc24ed7164f568506fb
Clemens Horn <clemens.horn@mni.thm.de>
2024-08-28 15:04:48 +0200

    chore: workspace

--- src/backend/src/main.rs
+++ src/backend/src/main.rs
@@ -1,3 +1,23 @@
-fn main() {
-    println!("Hello, world!");
+//! Run with
+//!
+//! ```not_rust
+//! cargo run
+//! ```
+
+use axum::{routing::get, Router};
+
+#[tokio::main]
+async fn main() {
+    tracing_subscriber::fmt::init();
+    let app = Router::new().route("/", get(root));
+
+    let listener = tokio::net::TcpListener::bind("127.0.0.1:3000")
+        .await
+        .unwrap();
+    tracing::debug!("listening on {}", listener.local_addr().unwrap());
+    axum::serve(listener, app).await.unwrap();
+}
+
+async fn root() -> &'static str {
+    "Hello, World!"
 }
