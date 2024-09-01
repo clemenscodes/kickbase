64b01e05bbabae54b8cc34659df04c34b6cee65b
Clemens Horn <clemens.horn@mni.thm.de>
2024-09-01 18:01:40 +0200

    chore(logging): update logic

--- src/server/router/routes/assets/mod.rs
+++ src/server/router/routes/assets/mod.rs
@@ -1,14 +1,11 @@
 use axum::Router;
 use tower_http::services::ServeDir;
-use tracing::{error, warn};
+use tracing::{debug, error};
 
 pub fn router() -> Router {
   let path = std::env::var("KICKBASE_ASSETS").map_or_else(
-        |env_err| {
-            warn!(
-                "KICKBASE_ASSETS environment variable is not set or could not be read: {}. Falling back to current directory.",
-                env_err
-            );
+        |_| {
+            debug!("KICKBASE_ASSETS environment variable is not set. Using current directory as the fallback for assets.");
             std::env::current_dir()
                 .map(|path| path.join("assets"))
                 .unwrap_or_else(|err| {
@@ -16,7 +13,10 @@ pub fn router() -> Router {
                     std::process::exit(1);
                 })
         },
-        std::path::PathBuf::from
+        |path_str| {
+            debug!("KICKBASE_ASSETS environment variable found, using path: {}", path_str);
+            std::path::PathBuf::from(path_str)
+        }
     );
 
   let assets = path.to_str().unwrap_or_else(|| {
